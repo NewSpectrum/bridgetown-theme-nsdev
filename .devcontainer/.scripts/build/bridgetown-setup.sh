3ba9c64cc82fdd6464d5039884a6e6d2efaf075b
Richard Joubert <rjoubert@nsd.team>
2023-05-11 04:04:34 -0400

    update
    Signed-off-by: Richard Joubert <rjoubert@nsd.team>

--- .devcontainer/.scripts/build/bridgetown-setup.sh
+++ .devcontainer/.scripts/build/bridgetown-setup.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/bash/env bash
+# shellcheck disable=SC1090
 
-declare -gx btstartups="../startups"
+declare -gx btstartups="./.devcontainer/.scripts/startup"
 
 if [[ -z "$btown" ]]; then
-	. "../startups/aliases.sh"
-	
-	for script in ""
+	for script in "${btstartups}"/*.sh; do
+		source "$script"
+	done
+fi
