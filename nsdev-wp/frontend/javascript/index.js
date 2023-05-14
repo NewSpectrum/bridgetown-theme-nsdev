c2db88b78f387e6f81742a038967a201f91b42ca
Richard Joubert <rjoubert@nsd.team>
2023-05-14 02:50:49 -0400

    webpack commit
    Signed-off-by: Richard Joubert <rjoubert@nsd.team>

---
+++ nsdev-wp/frontend/javascript/index.js
@@ -0,0 +1,8 @@
+import "index.scss"
+import "syntax-highlighting.css"
+
+// Import all JavaScript & CSS files from src/_components
+const componentsContext = require.context("bridgetownComponents", true, /\.(js|css)$/)
+componentsContext.keys().forEach(componentsContext)
+
+console.info("Bridgetown is loaded!")
